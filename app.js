323e6c3e11929d888e1052d38c5317d132c18c69
Tasha <tashadifa@gmail.com>
2021-03-17 21:07:14 +0700

    Updated validation

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const valid = firstName.checkValidity();
+const form = document.querySelector('form');
+const nameInput = document.querySelector('input[name]');
+
+const validateInputs = () => {
+  if (!nameInput.value) {
+    nameInput.classList.add('invalid');
+    nameInput.nextElementSibling.classList.remove('hidden');
+  }
+};
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  validateInputs();
+});
