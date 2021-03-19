be9e491183875a76e2aff04ac2aaec16956dbcbf
Tasha <tashadifa@gmail.com>
2021-03-19 19:48:06 +0700

    Updated app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-let form = document.getElementById('form');
-let firstName = document.getElementById('firstName');
-let lastName = document.getElementById('lastName');
-let email = document.getElementById('email');
-let password = document.getElementById('new-password');
+const form = document.getElementById('form');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const password = document.getElementById('new-password');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
 });
 
-function checkValid() {}
+function validateInputs() {
+  firstNameValue = firstName.value.trim();
+  lastNameValue = lastName.value.trim();
+  emailValue = email.value.trim();
+  passwordValue = password.value.trim();
+}
