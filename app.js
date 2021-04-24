183a12fe5d87bee5a2bf2f4808ade9b3612eba1d
Tasha Syarifah <tashadifa@gmail.com>
2021-04-24 21:56:53 +0700

    Updated validate function

--- app.js
+++ app.js
@@ -1,46 +1,22 @@
-const form = document.getElementById('form');
-const firstName = document.getElementById('firstName');
-const lastName = document.getElementById('lastName');
-const email = document.getElementById('email');
-const password = document.getElementById('new-password');
+const form = document.querySelector('form');
+const firstName = document.querySelector('input[name="firstName"]');
+const lastName = document.querySelector('input[name="lastName"]');
 
-const email_reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const pass_reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+const isValidEmail = (email) => {
+  const RegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return RegExp.test(String(email).toLowerCase());
+};
 
-form.addEventListener('submit', validateInputs);
+const isValidPassword = (password) => {
+  const RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+  return RegExp.test(String(password).toLowerCase());
+};
 
-function validateInputs(e) {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
+  console.log("haa");
+})
 
-  firstNameValue = firstName.value.trim();
-  lastNameValue = lastName.value.trim();
-  emailValue = email.value.trim();
-  passwordValue = password.value.trim();
 
-  if (firstNameValue === '') {
-    checkError(firstNameValue, 'First Name cannot be empty');
-  }
 
-  if (lastNameValue === '') {
-    checkError(lastNameValue, 'Last Name cannot be empty');
-  }
 
-  if (emailValue === '') {
-    checkError(emailValue, 'Email cannot be empty');
-  } else if(!email_reg.test(emailValue)) {
-    checkError(emailValue, 'Email is not validate');
-  }
-
-  if (passwordlValue === '') {
-    checkError(passwordValue, 'Password cannot be empty');
-  } else if (!pass_reg.test(passwordValue)) {
-    checkError(passwordValue, 'Password min 8 characters');
-  } 
-}
-
-function checkError(input, errorText) {
-  let field = input.parentElement;
-  field.classList.add('.field__error');
-  let text = field.querySelector('.field__text');
-  text.innerText = errorText;
-}
