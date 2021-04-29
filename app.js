fc9749676f97c6aca22aa1397f6d77e23b361990
Tasha Syarifah <tashadifa@gmail.com>
2021-04-29 20:34:06 +0700

    Updated js

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-
-const isValidEmail = (email) => {
-  const RegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return RegExp.test(String(email).toLowerCase());
-};
-
-const isValidPassword = (password) => {
-  const RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-  return RegExp.test(String(password).toLowerCase());
-};
-
 const form = document.querySelector('form');
 const fnInput = document.getElementsByName('firstName');
 const lnInput = document.getElementsByName('lastName');
 
-
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  
-});
+})
 
 
 
