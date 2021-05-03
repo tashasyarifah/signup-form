d699cec71a73333a17b3107f4b3a2549f6ae63f4
Tasha Syarifah <tashadifa@gmail.com>
2021-05-03 19:58:48 +0700

    Updated variables

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const form = document.querySelector('form');
-const fnInput = document.getElementsByName('firstName');
-const lnInput = document.getElementsByName('lastName');
+const form = document.getElementById('form');
+const fnInput = document.getElementById('firstName');
+const lnInput = document.getElementById('lastName');
+const emailInput = document.getElementById('email');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
